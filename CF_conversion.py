d891ef6e9042882907b48d3bbbb97970ce850e32
tcwoodward <53839046+tcwoodward@users.noreply.github.com>
2020-12-18 22:42:02 -0800

    Update CF_conversion.py
    Styling

--- CF_conversion.py
+++ CF_conversion.py
@@ -2,23 +2,18 @@
 This is a C/F calculator I made to help myself learn some of the basics of Python as is the first program/script
 I've created from scratch! It is meant to be ran in a terminal as an executed program.
 """
-
 # Start of program
-
 print("This is a calculator for converting Fahrenheit and Celsius between each other.")
 
 # Variables
-
 F, C = "Fahrenheit", "Celsius"
 prompt = "\nPlease enter your number for %s: "
 results = "\n%d %s is %d %s."
 
 # Begin program
-
 x = input("\nPlease choose Fahrenheit or Celsius (F or C): ")
 
 # Conditional Statements and Maths
-
 if (x == "F" or x == "f"):
     f = float(input(prompt%F))
     c = (f - 32) / 1.8
@@ -31,5 +26,4 @@ else:
     print("\nPlease restart the calculator and try again.")  
     
 input("\nPress any key to quit.")    
-
 # End of program  
